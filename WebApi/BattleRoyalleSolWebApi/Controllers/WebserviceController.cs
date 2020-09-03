4b3be99eb7d9ce36f666810cbba5e1e032e19261
Henrique Matias <henrique.matias@secrel.net.br>
2020-09-03 14:48:12 -0300

    Adiciona controller do webservice para webapi

---
+++ WebApi/BattleRoyalleSolWebApi/Controllers/WebserviceController.cs
@@ -0,0 +1,25 @@
+﻿using Infrastructure.Websockets;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BattleRoyalleSolWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class WebserviceController : ControllerBase
+    {
+        private readonly IWebSocketService _websocketService;
+
+        public WebserviceController(IWebSocketService websocketService) => _websocketService = websocketService;
+
+        [HttpPost]
+        public IActionResult SendCommand(string socketId, string message)
+        {
+            return Ok(_websocketService.SendCommand(socketId, message));
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_websocketService.ListAllSockets());
+        }
+    }
+}
