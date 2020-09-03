ad0514f0cad1b839fe73a8fb087a2a1e3f8d90b1
Henrique Matias <henrique.matias@secrel.net.br>
2020-09-03 14:54:33 -0300

    Adiciona serviços para obtenção do objeto com informações do servidor

---
+++ WinService/Domain/Services/ServerInfoService.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Builder;
+using Domain.Entities;
+
+namespace Domain.Services
+{
+    public class ServerInfoService : IServerInfoService
+    {
+        public ServerInfo GetServerInfo()
+        {
+            ServerInfoDirector director = new ServerInfoDirector();
+            ServerInfoBuilder builder = new ServerInfoBuilder();
+
+            director.Builder = builder;
+            director.BuildServerInfo();
+
+            return builder.GetServerInfo();
+        }
+    }
+}
