b8603eb2ca8407c48759a750b9cba5d024eece3e
Henrique Matias <henrique.matias@secrel.net.br>
2020-09-03 14:47:09 -0300

    Adiciona extensoes de middleware

---
+++ WebApi/BattleRoyalleSolWebApi/Extensions/WebSocketManagerExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Infrastructure.Handlers;
+using Infrastructure.Middleware;
+using Infrastructure.Websockets;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+using System.Reflection;
+
+namespace BattleRoyalleSolWebApi.Extensions
+{
+    public static class WebSocketManagerExtensions
+    {
+        public static IServiceCollection AddWebSocketManager(this IServiceCollection services)
+        {
+            services.AddTransient<ConnectionHandler>();
+
+            foreach (var type in Assembly.GetEntryAssembly().ExportedTypes)
+            {
+                if (type.GetTypeInfo().BaseType == typeof(WebsocketMannager))
+                {
+                    services.AddSingleton(type);
+                }
+            }
+
+            return services;
+        }
+
+        public static IApplicationBuilder MapWebSocketManager(this IApplicationBuilder app,
+                                                              PathString path,
+                                                              IWebSocketService handler)
+        {
+            return app.Map(path, (_app) => _app.UseMiddleware<WebSocketMiddleware>(handler.GetWebsocketMannager()));
+        }
+    }
+}
